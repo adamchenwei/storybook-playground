17d20ca3f0a3d9cfa3e714714036aadfaaa4997c
adamchenwei <awei@pebblepost.com>
2018-03-22 14:55:35 -0400

    chore(): cleanup

--- storyshot/.storybook/config.js
+++ storyshot/.storybook/config.js
@@ -1,7 +1,5 @@
 import { configure } from '@storybook/react';
 const req = require.context('../src', true, /\.stories\.js$/);
-// import initStoryshots from '@storybook/addon-storyshots';
-// initStoryshots();
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
